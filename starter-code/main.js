5a8c92dbc0a0434fe0f86dfed96ab7a97c7c1dc3
ivannash23 <ivannash23@Gmail.com>
2017-01-21 03:24:13 -0800

    if statements createdc

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,3 +2,10 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+if (cardTwo === cardFour || cardTwo === cardThree || cardOne === cardThree || cardOne === cardFour) {
+	alert("Sorry, try again.");
+}
+if (cardTwo === cardOne || cardThree === cardFour) {
+	alert("You found a match!");
+}
